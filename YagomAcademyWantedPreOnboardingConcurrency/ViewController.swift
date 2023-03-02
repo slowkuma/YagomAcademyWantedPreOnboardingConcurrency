d79d420ae3a5c246bef09d37f691dd9df4b5ac61
Martin Jeong <slowkuma@gmail.com>
2023-03-02 10:38:36 +0900

    Initial Commit

---
+++ YagomAcademyWantedPreOnboardingConcurrency/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YagomAcademyWantedPreOnboardingConcurrency
+//
+//  Created by Kyu Yeol Jeong on 2023/03/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
